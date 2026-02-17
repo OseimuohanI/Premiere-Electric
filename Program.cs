12d7c51ff48dfe65f282cec6e372f63179d2635e
Oseimuohan Itua <oseimuohanitua@outlook.com>
2026-02-17 06:17:17 +0100

    Add GitHub Pages URL to CORS policy
    - Allow requests from https://oseimuohani.github.io

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "https://charming-paprenjak-ca099d.netlify.app",
+            "https://oseimuohani.github.io",
             "http://localhost",
             "http://localhost:5500",
             "http://localhost:5000",
